dcbc7702eecb3df462dbe71bf866da6471d5f51e
salarmotevalli <salar.mo.ro@gmail.com>
2022-10-30 15:20:23 +0330

    render first html page

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+	mux := routes()
+
+	log.Println("server is running on port 8080")
 	
+	_ = http.ListenAndServe(":8080", mux)
 }
 
---
+++ cmd/web/routes.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"ws/internal/handlers"
+	"net/http"
+	"github.com/bmizerany/pat"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	return mux
+}
